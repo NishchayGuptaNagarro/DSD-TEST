1ea29539e8767af54abbbd22b47771591d22b655
Anubhav <anubhav.bhardwaj@nagarro.com>
2024-04-01 16:49:58 +0530

    timeline-contextapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.scss';
-import {createTheme, CssBaseline, ThemeProvider} from '@mui/material';
+import {createTheme, CssBaseline, Select, ThemeProvider} from '@mui/material';
 import Login from './screens/Login/Login.tsx';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import {lazy, Suspense} from 'react';
 import Loading from './screens/Loading/Loading.tsx';
-// const Sidebar = lazy(() => import('./component/Sidebar/Sidebar'));
-// const Breadcrump = lazy(() => import('./component/Breadcrump/Breadcrump'));
+import TimeLineState from './context/timeline/TimelineState.tsx';
 
 const AvailableStock = lazy(
   () => import('./screens/AvailableStock/AvailableStock.tsx'),
@@ -14,6 +13,19 @@ const AvailableStock = lazy(
 const ForgotPassword = lazy(
   () => import('./screens/ForgotPassword/ForgotPassword.tsx'),
 );
+
+const TimeLine = lazy(() => import('./component/Timeline/Timeline.tsx'));
+const SelectDriver = lazy(
+  () => import('./component/Timeline/SelectDriver.tsx'),
+);
+const Verification = lazy(
+  () => import('./component/Timeline/Verification.tsx'),
+);
+
+const ActiveDriver = lazy(
+  () => import('./component/Timeline/ActiveDriver.tsx'),
+);
+
 function App() {
   const theme = createTheme({
     typography: {
@@ -28,31 +40,66 @@ function App() {
   return (
     <>
       {/*Make sure all components using material ui goes inside this*/}
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          {/*this baseline provides grey background used in all screens */}
-          <CssBaseline />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <AvailableStock />
-                </Suspense>
-              }
-            />
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/forgotpassword"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <ForgotPassword />
-                </Suspense>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
+      <TimeLineState>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            {/*this baseline provides grey background used in all screens */}
+            <CssBaseline />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Suspense fallback={<Loading />}>
+                    <AvailableStock />
+                  </Suspense>
+                }
+              />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/forgotpassword"
+                element={
+                  <Suspense fallback={<Loading />}>
+                    <ForgotPassword />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/timeline"
+                element={
+                  <Suspense fallback={<Loading />}>
+                    <TimeLine />
+                  </Suspense>
+                }>
+                <Route
+                  path="select"
+                  element={
+                    <Suspense fallback={<Loading />}>
+                      <SelectDriver />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="verification"
+                  element={
+                    <Suspense fallback={<Loading />}>
+                      <Verification />
+                    </Suspense>
+                  }
+                />
+
+                <Route
+                  path="activation"
+                  element={
+                    <Suspense fallback={<Loading />}>
+                      <ActiveDriver />
+                    </Suspense>
+                  }
+                />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </TimeLineState>
     </>
   );
 }
---
+++ src/context/timelineProps/timeline.type.ts
@@ -0,0 +1,15 @@
+import {Dispatch, SetStateAction} from 'react';
+
+interface TimeLineContextProps {
+  steps: string[];
+  currentStep: number;
+  complete: boolean;
+  setCurrentStep: Dispatch<SetStateAction<number>>;
+  setComplete: Dispatch<SetStateAction<boolean>>;
+  stepsComplete:()=>void;
+  increaseSteps:()=>void;
+  decreaseSteps:()=>void;
+  rememberSteps:()=>void;
+}
+
+export default TimeLineContextProps;
