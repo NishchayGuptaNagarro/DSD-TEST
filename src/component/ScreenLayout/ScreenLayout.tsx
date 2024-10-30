45577008ebd4db8525f86cacb04b7b68860b9672
Ayush Srivastava <ayush.srivastava04@nagarro.com>
2024-10-30 13:00:01 +0530

    feat:[AEEE-868] Resolved comments

--- src/component/ScreenLayout/ScreenLayout.tsx
+++ src/component/ScreenLayout/ScreenLayout.tsx
@@ -5,13 +5,13 @@ import {ScreenLayoutProps} from './propTypes/types.ts';
 function ScreenLayout({children}: ScreenLayoutProps) {
   return (
     <Grid container>
-      <Grid height={'100vh'} item xs={3} padding={0.3}>
+      <Grid height={'100vh'} item xs={2.6} padding={0.3}>
         <Sidebar />
       </Grid>
       <Grid
         className={'avl-stock-screen hide-scrollbar'}
         item
-        xs={9}
+        xs={9.4}
         minHeight={400}
         sx={{maxHeight: '100vh', overflowY: 'scroll'}}>
         {children}
