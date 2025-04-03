3e6a0521b17c0c046d0369769ac2da37d822c609
Aeshu Chauhan <aeshu.chauhan@nagarro.com>
2025-04-03 11:45:42 +0530

    FIX git comment

--- src/utilities/timelineRoutes.ts
+++ src/utilities/timelineRoutes.ts
@@ -1,7 +1,7 @@
 export const vanSellerRoutes = ['driver', 'order', 'signature'];
 export const hybridRoutes = ['driver', '404', '404', '404', '404'];
 
-export const deliveryRoutes = ['driver', 'my-order', 'delivery-table', 'signature','404'];
+export const deliveryRoutes = ['driver', 'my-order', 'delivery-table', 'signature'];
 
 export const checkInRoutes = [
   'driver',
